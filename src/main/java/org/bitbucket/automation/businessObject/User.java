eb9a7802e86bb88cf88e1f5b30c5948d8538f446
Andrei Sakolchyk <Adrei_Sakolchyk@epam.com>
2016-05-22 22:54:16 +0300

    Revert "1. Added code"
    This reverts commit 1761c04c948e6cc697868ab638cc8393a0a02711.

--- src/main/java/org/bitbucket/automation/businessObject/User.java
+++
@@ -1,9 +0,0 @@
-package org.bitbucket.automation.businessObject;
-
-public class User {
-
-    public final static String email = "epam_test@leeching.net";
-    //public final static String email = "vokuduje@leeching.net";
-    public final static String password = "qwerty123";
-
-}
